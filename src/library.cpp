610776627503759c01969e66ee05f7a89ca9d084
AlSleepless <bessonov853@gmail.com>
2026-08-16 08:06:17 +0000

    task 1 - unit test - green line

--- src/library.cpp
+++ src/library.cpp
@@ -1,1 +1,14 @@
 #include "library.hpp"
+
+std::string getFirstUnique(const std::vector<std::string> &words) {
+  std::map<std::string, std::size_t> wordCounter{};
+  for (auto &word : words) {
+    ++wordCounter[word];
+  }
+  for (auto &[word, count] : wordCounter) {
+    if (count == 1) {
+      return word;
+    }
+  }
+  return "";
+}
--- src/library.hpp
+++ src/library.hpp
@@ -1,4 +1,19 @@
 #ifndef TRITONEELECTRONNIKS_TEST_ASSIGNMENT
 #define TRITONEELECTRONNIKS_TEST_ASSIGNMENT
 
+#include <cstdint>
+#include <map>
+#include <string>
+#include <vector>
+
+/*  Task 1  */
+/*
+Задача № 1. Дан std::vector<std::string> words. Напишите быструю функцию,
+которая не изменяя исходный массив вернет первую строку, встречающуюся в векторе
+ровно один раз. Если таких нет, верните "" (написание функции main, заполнение
+массива можно опустить, оставив только подключаемые файлы и функцию).
+*/
+
+std::string getFirstUnique(const std::vector<std::string> &words);
+
 #endif
