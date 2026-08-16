c95e4c5c84a008206fe438642b6a10354fa1e385
AlSleepless <bessonov853@gmail.com>
2026-08-16 07:59:06 +0000

    task1 - unit test - redline

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -1,3 +1,15 @@
 #include "library.hpp"
 
 #include <gtest/gtest.h>
+
+TEST(Task1, Positive1) {
+  std::vector<std::string> testCase{"word1", "word2", "word3"};
+  std::string res = getFirstUnique(testCase);
+  ASSERT_EQ(res, "word1");
+}
+
+TEST(Task1, Positive2) {
+  std::vector<std::string> testCase{"word1", "word2", "word3", "word1"};
+  std::string res = getFirstUnique(testCase);
+  ASSERT_EQ(res, "word2");
+}
