e057ef12ffa1f9e5291a5d0a20d1983a5e3b22b8
AlSleepless <bessonov853@gmail.com>
2026-08-16 10:56:39 +0000

    task 2 - unit test - redline

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -19,3 +19,27 @@ TEST(Task1, Negative1) {
   std::string res = getFirstUnique(testCase);
   ASSERT_EQ(res, "");
 }
+
+TEST(Task2, Positive1) {
+  Worker worker("Jhon Smit");
+  ASSERT_EQ(worker.name, "Jhon Smit");
+}
+
+TEST(Task2, Positive2) {
+  auto worker{createWorker("Jhon Smit")};
+  ASSERT_NE(worker, nullptr);
+}
+
+TEST(Task2, Positive3) {
+  auto worker{createWorker("Jhon Smit")};
+  std::string res = worker->getNameInLowerCase();
+  ASSERT_EQ(res, "jhon smit");
+}
+
+TEST(Task2, Positive4) {
+  auto worker{createWorker("Jhon Smit")};
+  testing::internal::CaptureStdout();
+  worker->onDone();
+  std::string output = testing::internal::GetCapturedStdout();
+  ASSERT_EQ(output, "task Jhon Smit completed.\n");
+}
