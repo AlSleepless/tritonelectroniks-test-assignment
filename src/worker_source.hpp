d01c6707993dac132bd5cd542ba334da615f2360
AlSleepless <bessonov853@gmail.com>
2026-08-16 08:28:28 +0000

    task 2 - source

---
+++ src/worker_source.hpp
@@ -0,0 +1,45 @@
+/*  Task 2  */
+/*
+Задача № 2.  Дан следующий код:
+Вопросы:
+1. Какие проблемы имеются в коде?
+2. Если да, тогда объясните механизм возникновения, предложите исправление либо
+опишите словами, чтобы вы исправили.
+*/
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+
+struct Worker {
+  std::string name;
+  std::function<void()> onDone;
+
+  explicit Worker(std::string n) : name{n} {}
+
+  std::string &getNameInLowerCase() {
+    std::string lc{name};
+    std::transform(lc.begin(), lc.end(), lc.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+    return lc;
+  }
+};
+
+std::shared_ptr<Worker> createWorker(std::string name) {
+  auto worker{std::make_shared<Worker>(name)};
+  worker->onDone = [worker]() {
+    std::cout << "Task " << worker->name << " completed.\n";
+  };
+
+  return worker;
+}
+
+int main() {
+  auto worker{createWorker("IDLE")};
+
+  std::cout << "Worker name: " << worker->getNameInLowerCase() << std::endl;
+  worker->onDone();
+
+  return 0;
+}
